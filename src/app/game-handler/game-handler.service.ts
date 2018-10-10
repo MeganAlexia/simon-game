c3bcdd63976372026412f34de3c9f60d1d833619
MeganAlexia <megan.arneau@gmail.com>
2018-10-10 21:02:21 +0200

    feat: if mistake loose don't wait for user to finish the sequence

--- src/app/game-handler/game-handler.service.ts
+++ src/app/game-handler/game-handler.service.ts
@@ -4,15 +4,15 @@ import { randomInt } from '../helpers/javascript.helper';
 
 @Injectable()
 export class GameHandlerService {
-  sequence = [];
-  sequenceReproduced = [];
-  iterations = 1;
-  level = 0;
-  score = 0;
-  disabledButton = true;
-  disabledStart = false;
+  private sequence = [];
+  private iterations = 1;
+  private level = 0;
+  private score = 0;
+  private disabledButton = true;
+  private disabledStart = false;
+  private advancement = 1;
 
-  start(): void {
+  public start(): void {
     const max = BUTTONS.length;
     for (let i = 0; i < this.iterations; i++) {
       this.sequence.push(randomInt(max));
@@ -23,35 +23,39 @@ export class GameHandlerService {
     console.log('Sequence emitted by Simon', this.sequence);
   }
 
-  handleChange(value): void {
-    this.sequenceReproduced.push(parseInt(value, 0));
+  public handleChange(value): void {
+    const end = this.advancement === this.sequence.length;
+    const equal = value === this.sequence[this.advancement - 1];
+    
+    // TODO add success popin instead of console log
 
-    const reproducedLength = this.sequenceReproduced.length;
-    const goalLength = this.sequence.length;
-    if (reproducedLength !== goalLength) {
-      // user didn't finish to click
-      return;
-    }
-
-    if (JSON.stringify(this.sequenceReproduced) === JSON.stringify(this.sequence)) {
+    if (end) {
+      console.log('you win');
       // user win
       this.level += 1;
       this.score += 10;
       this.iterations += 1;
-    } else {
+      this.cleanLayout();
+      return;
+    }
+
+    if (!equal) {
+      console.log('you loose');
       this.iterations = 1;
       this.level = 0;
       this.score = 0;
+      this.cleanLayout();
+      return;
+    } else {
+      this.advancement = this.advancement + 1;
     }
-
-    this.cleanLayout();
   }
 
   private cleanLayout() {
     this.disabledButton = true;
     this.disabledStart = false;
+    this.advancement = 1;
     this.sequence = [];
-    this.sequenceReproduced = [];
   }
 
 }
